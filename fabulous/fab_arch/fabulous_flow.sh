69c16ee7ef1f70388ab1a53ef13402b09b7db211
jingyu-uom <jing.yu@manchester.ac.uk>
2021-10-12 11:20:13 +1100

    debug

--- fabulous/fab_arch/fabulous_flow.sh
+++ fabulous/fab_arch/fabulous_flow.sh
@@ -2,6 +2,6 @@
 set -ex
 yosys -p "tcl ../synth/synth_fabulous.tcl 4 ${1} ${1}.json" ${1}.v -l ${1}_yosys_log.txt
 sed 's/sequential_16bit/'${1}'/g' bitstream_temp.py>bitstream.py
-${NEXTPNR:-../../nextpnr-fabulous} --pre-pack fab_arch.py --pre-place fab_timing.py --json ${1}.json --router router2 --router2-heatmap ${1}_heatmap --post-route bitstream.py --write pnr${1}.json --verbose --debug --log ${1}_npnr_log.log
+${NEXTPNR:-../../nextpnr-fabulous} --pre-pack fab_arch.py --pre-place fab_timing.py --json ${1}.json --router router2 --post-route bitstream.py --write pnr${1}.json --verbose --log ${1}_npnr_log.log
 python3 bit_gen.py -genBitstream ${1}.fasm meta_data_v2.txt ${1}_output.bin
 #yosys -p "read_verilog -lib ../synth/prims.v; read_json pnrblinky.json; dump -o blinky.il; show -format png -prefix blinky"
